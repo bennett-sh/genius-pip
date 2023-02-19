a11122502f99cd1a9ba5ce2110313cb2cf11e587
Bennett <110846042+bennett-sh@users.noreply.github.com>
2023-02-19 04:04:23 +0100

    Add update url

--- genius-pip.user.js
+++ genius-pip.user.js
@@ -6,10 +6,12 @@
 // @author       BSH
 // @match        https://genius.com/*-lyrics*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=genius.com
+// @downloadURL  https://github.com/bennett-sh/genius-pip/raw/main/genius-pip.user.js
 // @grant        none
 // ==/UserScript==
 
 const $ = s => document.querySelector(s)
+const $$ = s => document.querySelectorAll(s)
 
 let oldLyrics
 let lyrics
